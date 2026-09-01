cdeb1e449621e4650ae0cfe5d2d384284d2a63af
eunju <228353885+jeunju528-sudo@users.noreply.github.com>
2026-09-01 23:26:53 +0900

    feat: react-router 추가 및 로그인 페이지 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import { useState } from 'react'
-import axios from 'axios'
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.jsx'
+/* App.jsx : 최상위 컴포넌트
+  여기에다가 작은 컴포넌트들을 끼워 넣는 것!!
+*/
 function App() {
-  const [loginId, setLoginId] = useState("")
-  const [password, setPassword] = useState("")
-
-  const handleLogin = () => {
-    axios.post("http://localhost:8080/api/auth/login",
-      {
-        loginId,
-        password
-      }
-    ).then((response)=>{
-      localStorage.setItem("token", response.data.accessToken)
-    }).catch((error)=>{
-      console.log(error)
-    })
-  }
   return (
-    <div>
-      <h1>Login</h1>
-      
-      {/*
-        e = 이벤트 객체 (무슨 일이 일어났는지 정보)
-        e.target.value = 사용자가 입력창에 친 글자들
-      */}
-
-      <input type='text' value={loginId} onChange={(e)=>setLoginId(e.target.value)}></input>
-      <br />
-      <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-      <br />
-      <button type='button' onClick={handleLogin}>로그인</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' // 화면을 어떻게 그릴지 계산하는 담당
+import { createRoot } from 'react-dom/client' // 계산 결과를 실제 브라우저 DOM에 붙이는 담당
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' // 최상위 컴포넌트
+/* main.jsx : 리액트 앱 전체를 실제 웹페이지(html)에 연결하는 단 하나의 지점! */
+/* createRoot : react앱을 실제 html에 붙이는 함수 
+ createRoot(document.getElementById('root')) : index.html 파일의 id가 root 인 것을 찾아서 그 자리를 react가 관리할 루트로 지정
+ .render : 그 루트안에 App 를 그려 넣음
+  StrictMode : 개발 중에 작동하는 안전검사기, 화면에 아무것도 그리지는 않고 코드에 잠재적 문제가 있을 시 콘솔에 경고를 띄워주는 역할을 함
+ */
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render( 
+  <StrictMode> 
     <App />
   </StrictMode>,
 )
